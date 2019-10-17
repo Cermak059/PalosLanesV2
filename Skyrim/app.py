da438cf70b25856110b472bbb31fd24517e28e4c
Cermak059 <38162852+Cermak059@users.noreply.github.com>
2019-10-17 17:41:12 -0500

    Add GET request for user information

--- Skyrim/app.py
+++ Skyrim/app.py
@@ -5,6 +5,7 @@ import json
 import re
 import pymongo
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 from paschema import UserSchema
 from pacrypto import EncryptPassword, \
                      VerifyPassword
@@ -13,52 +14,93 @@ app = Flask(__name__)
 api = Api(app)
 
 #setup initial connection to MongoDB cluster and DB
-cluster = MongoClient("mongodb+srv://Cermak059:Pieman1993!@cluster0-nuw5p.mongodb.net/test?retryWrites=true&w=majority")
-db = cluster["PalosTest"]
+client = MongoClient("mongodb+srv://Cermak059:Pieman1993!@cluster0-nuw5p.mongodb.net/test?retryWrites=true&w=majority")
+db = client["PalosTest"]
 collection = db["Users"]
-
-#Create user dictionary
-users = {}
 
 
 class Register(Resource):
     def post(self):
+
         #Instantiate schema and load requested data into dictionary
         schema = UserSchema()
         data=json.loads(request.data)
+
         #Create new user by loading data from dictionary into UserSchema
         new_user = schema.load(data)
-        #Uodate the users dictionary with new user after passing schema
-        users.update(new_user)
-        #Now validate formatting of user data with validate_Data method
-        UserSchema.validate_Data(users)
-        #Insert user into DB
-        collection.insert_one(users)
 
-        print(users)
-        return{'result' : 'User added'}, 201
+        #Now validate formatting of user data with validate_Data method
+        UserSchema.validate_Data(new_user)
+
+        #Check to see if username is taken
+        if collection.find_one({"Username": new_user['Username']}):
+            raise ValueError("Username already taken please try again")
+
+        #Check if user exists in DB
+        if collection.find_one({"Email": new_user['Email']}):
+            print("You already have an account")
+        else:
+            collection.insert_one(new_user)
+            print ("User added")
+
 
 class Login(Resource):
      def post(self):
           #Instantiate schema and load login data into dictionary
           schema = UserSchema()
           data = json.loads(request.data)
-          #Check user login data against schema
+
+          #Load user login data against schema
           check_user = schema.load(data, partial=("Fname","Lname","Birthdate","Phone","Email","League"))
-          #Create new variables for password verification
-          chck_Password = data['Password']
-          stored_Password = users['Password']
-          #Determine if stored and password to check match
-          #If so log in
-          if VerifyPassword(chck_Password, stored_Password):
+
+
+          #Find user in database with associated username
+          results = collection.find_one({"Username": check_user['Username']})
+
+
+          #Check if no results have been returned
+          if not results:
+              print ("No user found")
+              return
+
+          #Now check password to verify user and login
+          if VerifyPassword(check_user['Password'], results['Password']):
               print ("Login Successful")
           else:
-              print ("Login Failed")
+              print ("Invalid Password")
 
+
+
+class Users(Resource):
+    def get(self):
+
+        #Try to load JSON get request
+        data = json.loads(request.data)
+
+        #Check for matching username in collection
+        results = collection.find_one({"Username": data['Username']})
+
+        #If no results return error
+        if not results:
+            print("No user found")
+
+        #If passwords dont match return error
+        if not VerifyPassword(data['Password'], results['Password']):
+            print("Password invalid")
+
+        #Try to delete password and ID keys from dictionary
+        try:
+            del (results['Password'],results['_id'])
+        except KeyError:
+            print("Key not found in dictionary")
+
+        #Return user information to client
+        return results
 
 
 api.add_resource(Login, '/Login')
 api.add_resource(Register, '/Register')
+api.add_resource(Users, '/Users')
 
 
 if __name__ == '__main__':
