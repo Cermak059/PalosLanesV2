4f5d41458e7ac452efa63c1220ef7db312b9816c
Cermak059 <38162852+Cermak059@users.noreply.github.com>
2019-10-21 10:05:10 -0500

    Adding auth tokens

--- Skyrim/pacrypto.py
+++ Skyrim/pacrypto.py
@@ -8,3 +8,8 @@ def EncryptPassword(Password):
 #Verify stored password with password to be checked
 def VerifyPassword(chck_Password, stored_Password):
      return sha256_crypt.verify(chck_Password, stored_Password)
+
+#Taking all uppercase characters A-Z and digits and combining lists
+#Picks random item out of list for size times
+def GenerateToken(size):
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
