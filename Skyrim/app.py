deef4b617bb84ab572af580c2194e2dc7fdfab98
Cermak059 <38162852+Cermak059@users.noreply.github.com>
2019-10-25 13:14:18 -0500

    Updated error handling

--- Skyrim/app.py
+++ Skyrim/app.py
@@ -5,6 +5,7 @@ import pprint
 import json
 import re
 import pymongo
+from marshmallow import ValidationError
 from datetime import datetime
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
@@ -13,7 +14,6 @@ from paschema import UserSchema
 from pamongo import Authorization,\
                     collection,\
                     authCollection
-
 from pacrypto import EncryptPassword,\
                      VerifyPassword,\
                      GenerateToken
@@ -22,8 +22,7 @@ from pacrypto import EncryptPassword,\
 app = Flask(__name__)
 api = Api(app)
 
-
-apiClient = PaApi
+apiClient = PaApi()
 
 
 class Register(Resource):
@@ -36,27 +35,35 @@ class Register(Resource):
         try:
             data=json.loads(request.data)
         except Exception as e:
-            return apiClient.badRequest("Invalid json")
+            return "Invalid json", 400
 
         #Create new user by loading data from dictionary into UserSchema
-        new_user = schema.load(data)
+        try:
+            new_user = schema.load(data, partial=("Token",))
+        except ValidationError as err:
+            return err.messages, 400
 
         #Now validate formatting of user data with validate_Data method
-        UserSchema.validate_Data(new_user)
+        try:
+            UserSchema.validate_Data(new_user)
+        except ValidationError as err:
+            return err.messages, 400
 
         #Check to see if username is taken
         if collection.find_one({"Username": new_user['Username']}):
-            return apiClient.badRequest("Username already in use")
+            return "Username already in use", 400
 
         #Check if user exists in DB
         if collection.find_one({"Email": new_user['Email']}):
-            return apiClient.badRequest("You already have an account")
+            return "You already have an account", 400
         else:
             #Create timestamp and update new_user data for db entry
-            ts = datetime.utcnow().isoformat
+            ts = datetime.utcnow().isoformat()
             new_user.update(Timestamp = ts)
             collection.insert_one(new_user)
-            return apiClient.success("User added")
+            return "User added", 200
+
+        #TODO email verification
 
 
 class Login(Resource):
@@ -68,11 +75,13 @@ class Login(Resource):
         try:
             data = json.loads(request.data)
         except Exception as e:
-            return apiClient.badRequest("Invalid json")
+            return "Invalid json", 400
 
         #Load user login data against schema
-        check_user = schema.load(data, partial=("Fname","Lname","Birthdate","Phone","Email","League"))
-
+        try:
+            check_user = schema.load(data, partial=("Fname","Lname","Birthdate","Phone","Email","League","Token",))
+        except ValidationError as err:
+            return err.messages, 400
 
         #Find user in database with associated username
         results = collection.find_one({"Username": check_user['Username']})
@@ -80,11 +89,11 @@ class Login(Resource):
 
         #Check if no results have been returned
         if not results:
-            return apiClient.notFound("User not found")
+            return "User not found", 404
 
         #Now check password to verify user and login
         if not VerifyPassword(check_user['Password'], results['Password']):
-            return apiClient.badRequest("Invalid password")
+            return "Invalid password", 400
 
         #Pass generated token to variable
         token = GenerateToken(20)
@@ -98,37 +107,40 @@ class Login(Resource):
         authCollection.insert_one(insertData)
         return token
 
-        return apiClient.success("Login successful")
 
 
 class Users(Resource):
     def get(self):
 
+        schema = UserSchema()
+
         try:
             data = json.loads(request.data)
         except Exception as e:
-            return apiClient.badRequest("Invalid json")
+            return "Invalid json", 400
 
-        #TODO load into schema and return 404 if not found
-        if "Token" not in data:
-            return "Token not found"
+        #Load into schema and return 404 if not found
+        try:
+            auth = schema.load(data, partial=("Fname","Lname","Birthdate","Phone","Email","League","Username","Password",))
+        except ValidationError as err:
+            return err.messages, 404
 
         #Pass token to variable
-        token = data['Token']
+        token = auth['Token']
 
         #Use method to get associated username from given token
         username = Authorization(token)
 
         #If no token found return error 401
         if not username:
-            return apiClient.unAuthorized("User not authorized")
+            return apiClient.unAuthorized()
 
         #Check for matching username in collection
         results = collection.find_one({"Username": username})
 
         #If no results return error
         if not results:
-            return apiClient.notFound("User not found")
+            return "User not found", 404
 
         #Try to delete password and ID keys from dictionary
         try:
