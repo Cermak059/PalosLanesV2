5ca79be149da9c68b08da4487ee034431f7a877f
Cermak059 <38162852+Cermak059@users.noreply.github.com>
2019-10-28 12:28:46 -0500

    Method for creating expiration times

--- Skyrim/pacrypto.py
+++ Skyrim/pacrypto.py
@@ -1,4 +1,8 @@
 from passlib.hash import sha256_crypt
+from datetime import datetime, timedelta
+import random
+import string
+
 
 #Password hashing method
 def EncryptPassword(Password):
@@ -7,9 +11,17 @@ def EncryptPassword(Password):
 
 #Verify stored password with password to be checked
 def VerifyPassword(chck_Password, stored_Password):
-     return sha256_crypt.verify(chck_Password, stored_Password)
+    return sha256_crypt.verify(chck_Password, stored_Password)
 
 #Taking all uppercase characters A-Z and digits and combining lists
 #Picks random item out of list for size times
 def GenerateToken(size):
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(size))
+
+def getExpirationTime(hours=None, minutes=None):
+    if hours:
+        return (datetime.utcnow() + timedelta(hours=hours)).isoformat()
+    elif minutes:
+        return (datetime.utcnow() + timedelta(minutes=minutes)).isoformat()
+    else:
+        return datetime.utcnow().isoformat()
