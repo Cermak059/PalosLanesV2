71814367095dae6f0921ad64caf764047a48c5e1
Cermak059 <38162852+Cermak059@users.noreply.github.com>
2019-11-24 23:17:24 -0600

    Path update

--- Skyrim/paconfig.py
+++ Skyrim/paconfig.py
@@ -3,8 +3,8 @@ import os
 
 CRON_SLEEP_SECONDS = 86400
 
-VERIFY_EMAIL_TEMPLATE = "verifytemplate.txt"
-SUCCESS_TEMPLATE = "successtemplate.txt"
+VERIFY_EMAIL_TEMPLATE = "/var/PalosLanesV2/Skyrim/verifytemplate.txt"
+SUCCESS_TEMPLATE = "/var/PalosLanesV2/Skyrim/successtemplate.txt"
 
 
 
