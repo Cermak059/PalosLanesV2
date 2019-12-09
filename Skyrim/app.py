b11bda1400631a200b7ccf2dbf44c901aa239c3c
Cermak059 <38162852+Cermak059@users.noreply.github.com>
2019-12-09 15:14:57 -0600

    Fixes

--- Skyrim/app.py
+++ Skyrim/app.py
@@ -356,7 +356,7 @@ class ChangePassword(Resource):
         if not results:
             return apiClient.notFound("Email not found")
 
-        if not collection.update_one({"Email": results['Email']}, {set:{"Password": hashedPassword}}):
+        if not collection.update_one({"Password": hashedPassword}, {"Email": results['Email']})
             logger.error("Failed to update user {} with new password".format(results['Email']))
             return apiClient.internalServerError()
 
