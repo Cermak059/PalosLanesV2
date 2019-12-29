1299ba97fe89aedb0a2e482da9a9df21abdfe9ec
Cermak059 <38162852+Cermak059@users.noreply.github.com>
2019-12-28 19:43:38 -0600

    FIxes

--- Skyrim/app.py
+++ Skyrim/app.py
@@ -251,8 +251,6 @@ class Users(Resource):
         #Use method to get associated username from given token
         username = Authorization(authToken)
 
-        if None:
-            return apiClient.unAuthorized()
         
         #If no token found return error 401
         if not username:
