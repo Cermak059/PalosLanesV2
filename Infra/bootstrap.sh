b19ed628038968758ef4a554e4b80252e6af3568
Kyle Cermak <38162852+Cermak059@users.noreply.github.com>
2020-02-02 16:02:42 -0600

    test

--- Infra/bootstrap.sh
+++ Infra/bootstrap.sh
@@ -7,3 +7,23 @@ pip3 install passlib
 pip3 install flask-restplus
 pip3 install requests
 
+mkdir -p /var/PalosLanesV2/Skyrim
+cd /var/PalosLanesV2/Skyrim
+aws s3 cp s3://chicagolandbowlingservice.com-bootstrap/Skyrim . --recursive
+echo "[Unit]
+Description=PalosLanes
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=root
+ExecStart=/bin/python3 /var/PalosLanesV2/Skyrim/app.py
+
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/PalosLanes.service
+
+systemctl enable PalosLanes
+systemctl start PalosLanes
